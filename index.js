15278f7b52bd2b98bf8d6a13f5f001346aa5e2a5
nosib55 <lmnosib555@gmail.com>
2025-12-13 19:12:37 +0600

    chore: add middleware for json, cors, and cookies

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
+// --------------------------------
+// Middleware
+// --------------------------------
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+// --------------------------------
 // Root route
+// --------------------------------
 app.get("/", (req, res) => {
   res.send("ClubSphere Backend Running");
 });
 
+// --------------------------------
 // Start server
+// --------------------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
