c7aca46f66ddb7774a58216c6dc82489c01fa5cc
nosib55 <lmnosib555@gmail.com>
2025-12-15 13:48:11 +0600

    fixed all issu

--- index.js
+++ index.js
@@ -114,9 +114,9 @@ app.post("/auth", async (req, res) => {
     );
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+     httpOnly: true,
+                secure: process.env.NODE_ENV === 'production', 
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
